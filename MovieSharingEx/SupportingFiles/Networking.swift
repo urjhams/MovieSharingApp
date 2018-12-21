fb350a54878bfcb1bc9203aeaa86d9af475f819b
Quan Dinh <n.dinh@tdsoftware.de>
2018-12-21 16:59:44 +0100

    learn to use guard let

--- MovieSharingEx/SupportingFiles/Networking.swift
+++ MovieSharingEx/SupportingFiles/Networking.swift
@@ -19,23 +19,24 @@ public class Networking {
      - Warning: Should update the UI in the completion closure, because the process works in a different thread
      */
     public static func loadMovies(fromUrl url: URL?, completion: @escaping (_ resultDict: NSDictionary?) -> ()){
-        if let availavleUrl = url {
-            Alamofire.request(availavleUrl, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: nil).responseJSON { (response) in
-                if let statusCode = response.response?.statusCode {
-                    switch statusCode {
-                    case Constants.YoutubeApi.NetworkStatusCode.ok:
-                        if let json = response.value as? NSDictionary {
-                            completion(json)
-                        }
-                    case Constants.YoutubeApi.NetworkStatusCode.badRequest:
-                        print("bad request")
-                    case Constants.YoutubeApi.NetworkStatusCode.forbidden:
-                        print("forbidden")
-                    case Constants.YoutubeApi.NetworkStatusCode.notFound:
-                        print("not found")
-                    default:
-                        break
+        guard let availableUrl = url else {
+            return
+        }
+        Alamofire.request(availableUrl, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: nil).responseJSON { (response) in
+            if let statusCode = response.response?.statusCode {
+                switch statusCode {
+                case Constants.YoutubeApi.NetworkStatusCode.ok:
+                    if let json = response.value as? NSDictionary {
+                        completion(json)
                     }
+                case Constants.YoutubeApi.NetworkStatusCode.badRequest:
+                    print("bad request")
+                case Constants.YoutubeApi.NetworkStatusCode.forbidden:
+                    print("forbidden")
+                case Constants.YoutubeApi.NetworkStatusCode.notFound:
+                    print("not found")
+                default:
+                    break
                 }
             }
         }
